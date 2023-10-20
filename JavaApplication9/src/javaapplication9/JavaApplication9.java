67fc1af07728005279cd0f5476e2be0075860ee1
Jason <Jason@DESKTOP-3RJEK5U>
2023-10-20 23:12:59 +0200

    Primer Commit

---
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,16 @@
+package javaapplication9;
+
+/**
+ *
+ * @author Jason.
+ */
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
