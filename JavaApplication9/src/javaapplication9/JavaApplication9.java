16ba7ec3d904f851f4bcefb15af1d7472b4421c1
Jason <Jason@DESKTOP-3RJEK5U>
2023-10-20 23:40:22 +0200

    Rama 2

--- JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -10,7 +10,7 @@ public class JavaApplication9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Rama Jason 2");
     }
 
 }
